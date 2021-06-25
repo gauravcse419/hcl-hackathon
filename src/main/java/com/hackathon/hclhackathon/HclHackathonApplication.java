139be961cc319734af9e09bdd81b804834f3a824
gauravcse419 <gauravcse419@gmail.com>
2021-06-25 22:34:18 +0530

    initial set up

---
+++ src/main/java/com/hackathon/hclhackathon/HclHackathonApplication.java
@@ -0,0 +1,14 @@
+package com.hackathon.hclhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclHackathonApplication.class, args);
+		System.out.println("hello world..!!");
+	}
+
+}
